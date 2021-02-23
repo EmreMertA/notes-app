9dfcd265fb488c30bf5fb73186f82c7b3dc79609
EmreMertA <emremakdag55@gmail.com>
2021-02-24 01:54:55 +0300

    some improvements on ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, useDisclosure } from "@chakra-ui/react";
+import { Button, Heading, Stack, useDisclosure } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import ModalContainer from "./components/AddNote";
 import Note from "./components/Note";
@@ -6,15 +6,36 @@ import Container from "./Container";
 import { NoteContext } from "./contexts/NoteContext";
 
 const App = () => {
-  const { notes } = useContext(NoteContext);
+  const { sortedNotes } = useContext(NoteContext);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Container bgSrc="notesbg.svg">
-      {notes.map((note) => (
-        <Note id={note.id} title={note.title} note={note.note} value={note.value} />
-      ))}
-      <Button onClick={onOpen}>Open Modal</Button>
+      <Heading color="#065E77" mt={12}>
+        Notes App
+      </Heading>
+      <Button
+        bgColor="#01BAEF"
+        color="white"
+        _hover={{ bgColor: "teal" }}
+        onClick={onOpen}
+        mt={8}
+      >
+        Not Ekle
+      </Button>
+
+      <Stack spacing={5} my={10}>
+        {sortedNotes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            note={note.note}
+            value={note.value}
+          />
+        ))}
+      </Stack>
+
       <ModalContainer onOpen={onOpen} onClose={onClose} isOpen={isOpen} />
     </Container>
   );
--- src/contexts/NoteContext.js
+++ src/contexts/NoteContext.js
@@ -11,19 +11,40 @@ const NoteContextProvider = (props) => {
         "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.",
       value: 8,
     },
+    {
+      id: uuidv4(),
+      title: "Emre Mert Akdağğ",
+      note:
+        "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.",
+      value: 5,
+    },
+    {
+      id: uuidv4(),
+      title: "Emre Mert Akdağğ",
+      note:
+        "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.",
+      value: 1,
+    },
   ]);
 
   const addNote = (title, note, value) => {
     setNotes([...notes, { id: uuidv4(), title, note, value }]);
   };
+
   const deleteNote = (id) => {
     setNotes(notes.filter((note) => note.id !== id));
   };
+
   const updateNote = (id, updatedNote) => {
     setNotes(notes.map((note) => (note.id === id ? updatedNote : note)));
   };
+
+  const sortedNotes = notes.sort((a, b) => (a.value < b.value ? 1 : -1));
+
   return (
-    <NoteContext.Provider value={{ notes, addNote, deleteNote, updateNote }}>
+    <NoteContext.Provider
+      value={{ notes, addNote, deleteNote, updateNote, sortedNotes }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
